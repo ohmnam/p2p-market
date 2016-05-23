12bc621293e5413be9f8e798aec5a15c045ae5dc
Mikko Koski <mikkokos@gmail.com>
2016-05-23 11:45:50 +0300

    Middleware

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,95 +1,5 @@
 require_relative './common.rb'
 
-class JsRoutes
-  class Middleware
-
-    DEFAULT_ROUTE_FILES = [Rails.root.join("config/routes.rb")]
-
-    class << self
-
-      attr_writer :route_files
-
-      def route_files
-        @route_files ||= DEFAULT_ROUTE_FILES
-      end
-
-    end
-
-    def initialize(app)
-      @app = app
-      clear_cache
-    end
-
-    def call(env)
-      @cache = nil
-      verify_route_files!(self.class.route_files)
-      @app.call(env)
-    end
-
-    private
-
-    def cache_path
-      @cache_path ||= cache_dir.join("routes.yml")
-    end
-
-    def cache_dir
-      @cache_dir ||= Rails.root.join("tmp/cache")
-    end
-
-    def cache
-      @cache ||= begin
-        if cache_path.exist?
-          YAML.load_file(cache_path) || {}
-        else
-          {}
-        end
-      end
-    end
-
-    def clear_cache
-      File.delete(cache_path) if File.exist?(cache_path)
-    end
-
-    def save_cache(new_cache)
-      # path could be a symbolic link
-      FileUtils.mkdir_p(cache_dir) unless File.exists?(cache_dir)
-      File.open(cache_path, "w+") do |file|
-        file << new_cache.to_yaml
-      end
-    end
-
-    # Check if routes.js should be regenerated.
-    # ONLY REGENERATE when these conditions are met:
-    #
-    # # Cache file doesn't exist
-    # # Route files and cache size are different (files were removed/added)
-    # # Routes file has been updated
-    #
-    def verify_route_files!(route_files)
-      valid_cache = []
-      new_cache = {}
-
-      valid_cache.push cache_path.exist?
-      valid_cache.push route_files.uniq.size == cache.size
-
-      route_files.each do |path|
-        changed_at = File.mtime(path).to_i
-        valid_cache.push changed_at == cache[path]
-        new_cache[path] = changed_at
-      end
-
-      return if valid_cache.all?
-
-      binding.pry
-
-      save_cache(new_cache)
-
-      JsRoutes.generate!
-    end
-  end
-end
-
-
 Kassi::Application.configure do
   APP_CONFIG ||= ConfigLoader.load_app_config
 
@@ -167,6 +77,4 @@ Kassi::Application.configure do
   config.active_record.mass_assignment_sanitizer = :strict
 
   config.cache_store = :memory_store, { :namespace => "sharetribe-dev"}
-
-  config.middleware.use JsRoutes::Middleware
 end
